ca101cb51a020d24f7082d656c68ab6dfc5d4606
Caleb Schwarze <ces55739@bethel.edu>
2024-01-22 16:02:01 -0600

    first commit with an outline

---
+++ src/containers/PlaygroundDirectory/index.jsx
@@ -0,0 +1,26 @@
+import React, { lazy, Suspense } from 'react';
+
+// TODO - play around with MUI
+const PlaygroundDirectory = () => {
+    const queryParameters = new URLSearchParams(window.location.search);
+    const componentToRender = queryParameters.get("component");
+    // TODO - pull this out to a method
+    const LoadedComponent = componentToRender?.length > 1 && lazy(() => import(`../${componentToRender.charAt(0).toUpperCase() + componentToRender.substr(1).toLowerCase()}`));
+
+    return (
+        <>
+            <ul>
+                <li><a href='.?component=carousel'>Carousel</a></li>
+            </ul>
+
+            <Suspense fallback="LOADING!"> 
+                <h2>Preview</h2>
+                { 
+                    LoadedComponent && <LoadedComponent />
+                }
+            </Suspense>
+        </>
+    )
+}
+
+export default PlaygroundDirectory;
