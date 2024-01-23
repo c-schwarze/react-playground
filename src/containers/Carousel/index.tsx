413cdba71305f71eb733b60ae383969b8d6a0a26
Caleb Schwarze <ces55739@bethel.edu>
2024-01-23 12:19:43 -0600

    basic styling of the carousel to hold is form

--- src/containers/Carousel/index.tsx
+++ src/containers/Carousel/index.tsx
@@ -20,13 +20,17 @@ const Carousel = () => {
 
     return (
         <div className='carousel'>
-            <button className='btn-prev' onClick={() => setCurrentImageIndex(mod(currentImageIndex-1, imgs.length))}>{'<'}</button>
+            <div className='carousel-btn-div'>
+                <button className='carousel-btn btn-prev' onClick={() => setCurrentImageIndex(mod(currentImageIndex-1, imgs.length))}>{'<'}</button>
+            </div>
             <div className='carousel-img-holder'>
                 {
                     currentImage && (<img className="carousel-img" src={currentImage?.src} alt={currentImage?.altTxt || 'No alt text defined!'} />)
                 }
             </div>
-            <button className='btn-next' onClick={() => setCurrentImageIndex(mod(currentImageIndex+1, imgs.length))}>{'>'}</button>
+            <div className='carousel-btn-div'>
+                <button className='carousel-btn btn-next' onClick={() => setCurrentImageIndex(mod(currentImageIndex+1, imgs.length))}>{'>'}</button>
+            </div>
         </div>
     );
 }
